bc92c62751c59f51d7fac27170f2dbc0c637ddbf
Tanhop <tanhop1305@gmail.com>
2022-10-18 09:38:42 +0700

    add Flask

---
+++ saleapp/app/index.py
@@ -0,0 +1,17 @@
+from flask import render_template, request
+from app import sale_app
+from app import dao
+
+
+@sale_app.route('/')
+def index():
+    categories = dao.load_categories()
+    cate_id = request.args.get('category_id')
+    products = dao.load_products(category_id=cate_id)
+    return render_template('index.html',
+                           categories=categories,
+                           products=products)
+
+
+if __name__ == '__main__':
+    sale_app.run(debug=True)
